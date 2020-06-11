12f8e6f8ed6d587b0b14748dcf72d5b372a60dcb
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2020-06-11 12:07:11 +0200

    add ProductList component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="d-flex justify-center">
+    <h1>This is the product list!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
