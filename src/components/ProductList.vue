b6c75f9720c80561ac03c18221a0b3107e2019ac
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2020-06-11 16:07:54 +0200

    [refactor] move product data out of product list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -26,34 +26,13 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropType } from 'vue';
 export default Vue.extend({
-  data() {
-    return {
-      products: [
-        {
-          id: '1',
-          name: 'Brötchen',
-          count: 3,
-          done: false,
-          dueDate: new Date('2020-06-11T13:29:40Z'),
-        },
-        {
-          id: '2',
-          name: 'Joghurt',
-          count: 5,
-          done: true,
-          dueDate: new Date('2020-06-11T13:29:40Z'),
-        },
-        {
-          id: '3',
-          name: 'Käse',
-          count: 1,
-          done: false,
-          dueDate: new Date('2020-06-11T13:29:40Z'),
-        },
-      ] as Product[],
-    };
+  props: {
+    products: {
+      type: Object as PropType<Product[]>,
+      required: true,
+    },
   },
   methods: {
     getDateString(date: Date) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,43 @@
 <template>
-  <product-list></product-list>
+  <product-list :products="products"></product-list>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue';
 // @ is an alias to /src
 import ProductList from '@/components/ProductList.vue';
 
-export default {
+export default Vue.extend({
   name: 'Home',
   components: {
     ProductList,
   },
-};
+  data() {
+    return {
+      products: [
+        {
+          id: '1',
+          name: 'Brötchen',
+          count: 3,
+          done: false,
+          dueDate: new Date('2020-06-11T13:29:40Z'),
+        },
+        {
+          id: '2',
+          name: 'Joghurt',
+          count: 5,
+          done: true,
+          dueDate: new Date('2020-06-11T13:29:40Z'),
+        },
+        {
+          id: '3',
+          name: 'Käse',
+          count: 1,
+          done: false,
+          dueDate: new Date('2020-06-11T13:29:40Z'),
+        },
+      ] as Product[],
+    };
+  },
+});
 </script>
