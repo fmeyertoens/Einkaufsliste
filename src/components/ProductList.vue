21b4a77aa6885762ea9028989d3fe76e1e6142c8
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2020-06-11 15:48:03 +0200

    display products in a simple list
    add sample data

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,67 @@
 <template>
   <div class="d-flex justify-center">
-    <h1>This is the product list!</h1>
+    <v-list>
+      <v-list-item
+        v-for="product in products"
+        :key="product.id"
+        :value="product.id"
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ product.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{
+            `${product.count} - ${getDateString(product.dueDate)}`
+          }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-checkbox v-model="product.done"></v-checkbox>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      products: [
+        {
+          id: '1',
+          name: 'Brötchen',
+          count: 3,
+          done: false,
+          dueDate: new Date('2020-06-11T13:29:40Z'),
+        },
+        {
+          id: '2',
+          name: 'Joghurt',
+          count: 5,
+          done: true,
+          dueDate: new Date('2020-06-11T13:29:40Z'),
+        },
+        {
+          id: '3',
+          name: 'Käse',
+          count: 1,
+          done: false,
+          dueDate: new Date('2020-06-11T13:29:40Z'),
+        },
+      ] as Product[],
+    };
+  },
+  methods: {
+    getDateString(date: Date) {
+      return Intl.DateTimeFormat('de-DE', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false,
+      }).format(date);
+    },
+  },
+});
 </script>
