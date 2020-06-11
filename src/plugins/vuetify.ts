a6d4c90c0e1114fe3f628bad33400880cf8a05b4
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2020-06-11 11:47:31 +0200

    adjust theme colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,28 +1,28 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
-import de from "vuetify/src/locale/de";
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import de from 'vuetify/src/locale/de';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
     options: {
-      customProperties: true
+      customProperties: true,
     },
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
-        accent: "#82B1FF",
-        error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107"
-      }
-    }
+        primary: '#3f51b5',
+        secondary: '#009688',
+        accent: '#ffc107',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#03a9f4',
+        success: '#4caf50',
+      },
+    },
   },
   lang: {
     locales: { de },
-    current: "de"
-  }
+    current: 'de',
+  },
 });
