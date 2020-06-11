3ecb8c870bef3fc6658bf28ac99de863f0458f4e
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2020-06-11 19:48:27 +0200

    add product form component

---
+++ src/components/ProductForm.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-card>
+    <v-card-title> <span class="headline">Neues Produkt</span></v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-form @submit.prevent="onSubmit">
+          <v-text-field v-model="product.name" label="Name"></v-text-field>
+          <v-text-field
+            v-model.number="product.count"
+            label="Anzahl"
+            type="number"
+          ></v-text-field>
+          <v-menu
+            v-model="dateMenu"
+            :close-on-content-click="false"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              @input="dateMenu = false"
+            ></v-date-picker>
+          </v-menu>
+          <v-menu
+            v-model="timeMenu"
+            :close-on-content-click="false"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="time"
+                prepend-icon="mdi-clock-outline"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-model="time"
+              @input="timeMenu = false"
+            ></v-time-picker>
+          </v-menu>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="cancel">Abbrechen</v-btn>
+            <v-btn color="primary" depressed type="submit">Hinzufügen</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { getEmptyProduct } from '@/services/productFactory';
+import { v4 as uuidv4 } from 'uuid';
+
+export default Vue.extend({
+  props: {
+    product: {
+      type: Object as PropType<Product>,
+      default: () => {
+        return getEmptyProduct();
+      },
+    },
+  },
+  data() {
+    return {
+      dateMenu: false,
+      timeMenu: false,
+      date: this.product.dueDate.toISOString().substr(0, 10),
+      time: Intl.DateTimeFormat('de-DE', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+      }).format(this.product.dueDate),
+    };
+  },
+  methods: {
+    onSubmit() {
+      const newProduct: Product = {
+        ...this.product,
+        id: uuidv4(),
+        dueDate: new Date(`${this.date}T${this.time}`),
+      };
+      this.$emit('newProduct', newProduct);
+    },
+    cancel() {
+      this.$emit('cancel');
+    },
+  },
+});
+</script>
