0550f7f8ea96298ae36d7ace02564e3156e921a8
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2020-06-15 16:38:09 +0200

    manifest is built by @vue/cli-plugin-pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ['vuetify'],
+  pwa: {
+    themeColor: '#3f51b5',
+    manifestPath: 'manifest.webmanifest',
+    manifestOptions: {
+      short_name: 'Einkaufsliste',
+      name: 'Einkaufsliste',
+      description: 'Eine einfache Einkaufsliste',
+      icons: [
+        {
+          src: '/img/icons/cart-192x192.png',
+          type: 'image/png',
+          sizes: '192x192',
+        },
+        {
+          src: '/img/icons/cart-512x512.png',
+          type: 'image/png',
+          sizes: '512x512',
+        },
+      ],
+      start_url: '/',
+      background_color: '#fff',
+      display: 'standalone',
+      scope: '/',
+    },
+  },
 };
